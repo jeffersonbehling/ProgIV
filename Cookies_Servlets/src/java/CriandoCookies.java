88ba9491d513e3f2ffc1f89f340258019c62106e
Jefferson Vantuir <jefferson.behling@gmail.com>
2018-04-12 16:17:01 -0300

    Criar e Visualizar Cookie

---
+++ Cookies_Servlets/src/java/CriandoCookies.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author User
+ */
+public class CriandoCookies extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet CriandoCookies</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
+            Cookie cookie = new Cookie("nome", "Jefferson");
+            
+            // DEFINE TEMPO DO COOKIE (SEGUNDOS)
+            cookie.setMaxAge(60);
+            
+            // DEFINE SE A CONEXÃO PRECISA SER SEGURA PARA ACESSO (https OU ssl)
+            cookie.setSecure(false);
+            
+            // DEFINE UM COMENTÁRIO PARA O COOKIE
+            cookie.setComment("Meu nome");
+            
+            // ADICIONA O COOKIE AO NAVEGADOR
+            response.addCookie(cookie);
+            
+            out.println("Seu cookie foi criado com sucesso!<br>");
+            out.println("<a href=\"VisualizarCookies\">Clique aqui</a> para visualizar"
+                    + " o(s) cookie(s) criado(s)!");
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
